f9a5d751ce479a3c35868cd825bdbf092a5380e3
Aravind Sripathy <aravindsripathy@s-169-232-126-128.resnet.ucla.edu>
2018-01-19 22:09:04 -0800

    Set up server to listen on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("naga");
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  console.log('home');
+  res.send('naga');
+});
+
+app.listen(port, function() {
+  console.log('listening on: ' + port);
+});
