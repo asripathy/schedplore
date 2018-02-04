4fb1f95f31fb4d832a27f8e4b528c4474a84fecc
Rahul <rahul444@hotmail.com>
2018-02-03 17:27:51 -0800

    basic google places api call

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+var https = require('https')
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var google_key = 'AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I';
 
 app.get('/', function(req, res) {
   console.log('home');
-  res.send('naga');
+  getGooglePlaces(res);
 });
 
 app.listen(port, function() {
   console.log('listening on: ' + port);
 });
+
+function getGooglePlaces(res) {
+  https.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I&location=37.870921,-122.259079&radius=500', function(resp) {
+    var data = '';
+    
+    // A chunk of data has been recieved.
+    resp.on('data', function(chunk) {
+      data += chunk;
+    });
+    
+    // The whole response has been received. Print out the result.
+    resp.on('end', function() {
+      res.send(JSON.parse(data));
+    });
+    
+    }).on("error", function(err) {
+      console.log("Error: " + err.message);
+    });
+}
