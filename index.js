e7db7b3187e70ba86db58330bedf5be8e3a9867e
Aravind Sripathy <aravindsripathy@s-169-232-126-73.resnet.ucla.edu>
2018-02-21 15:05:39 -0800

    Finish lat lng code

--- index.js
+++ index.js
@@ -6,29 +6,40 @@ var google_key = 'AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I';
 
 app.get('/', function(req, res) {
   console.log('home');
-  getPlaces(res, '37.870921,-122.259079', 500, 'restaurant');
-  getLatLng('San Francisco');
+  getPlaces(res, 'Los Angeles', 500, 'restaurant');
+  // getLatLng('San Francisco');
 });
 
 app.listen(port, function() {
   console.log('listening on: ' + port);
 });
 
-// converts city to lat,lng string
-function getLatLng(city) {
-  var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I';
+function extractLatLng(data){
+  console.log(data);
+  if(data['status'] != 'OK'){
+    //zero results handling
+  }
+  else{
+    latlng = String(data['results'][0].geometry.location.lat) + ','+ String(data['results'][0].geometry.location.lng);
+    return latlng;
+  }
+}
 
+
+// converts city to lat,lng string
+function getLatLng(city, callback) {
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I';
   https.get(url, function(resp) {
     var data = '';
-    
+
     resp.on('data', function(chunk) {
       data += chunk;
     });
-    
+
     resp.on('end', function() {
-      console.log(JSON.parse(data));
+      callback(extractLatLng(JSON.parse(data)));
     });
-    
+
     }).on("error", function(err) {
       console.log("Error: " + err.message);
     });
@@ -36,8 +47,10 @@ function getLatLng(city) {
 }
 
 // wrapper function for getGooglePlaces
-function getPlaces(res, location, radius, type) {
-  getGooglePlaces(res, location, radius, type);
+function getPlaces(res, city, radius, type) {
+  getLatLng(city, function(latlng){
+    getGooglePlaces(res, latlng, radius, type);
+  });
 }
 
 // returns a list of places objects
@@ -45,15 +58,15 @@ function getGooglePlaces(res, location, radius, type) {
   var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDEPGdDuGRpSFSlQ1tXy5EIAosKAtp8f5I&location=' + location + '&radius=' + radius + '&type=' + type
   https.get(url, function(resp) {
     var data = '';
-    
+
     resp.on('data', function(chunk) {
       data += chunk;
     });
-    
+
     resp.on('end', function() {
       res.send(JSON.parse(data));
     });
-    
+
     }).on("error", function(err) {
       console.log("Error: " + err.message);
     });
