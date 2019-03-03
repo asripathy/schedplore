06c1b9714e74627e77191df9a4ef3da50d203db1
Aravind Sripathy <aravind.sripathy@gmail.com>
2019-03-02 19:19:30 -0800

    Updated CSS

--- client/src/App.js
+++ client/src/App.js
@@ -18,20 +18,13 @@ class App extends Component {
     this.state = {
       response: '',
       address: '',
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, "hours")),
-          title: "Test Event"
-        }
-      ],
+      events: [],
       selectedStartDate: '',
       selectedEndDate: ''
     }
 
     this.updateCalendar = this.updateCalendar.bind(this)
   }
-  
 
   componentDidMount() {
     // this.callApi()
@@ -58,10 +51,8 @@ class App extends Component {
       const response = await fetch('/place/' + city);
       const body = await response.json();
       console.log(body);
-
       if (response.status !== 200) throw Error(body.message);
       this.setState({response: JSON.stringify(body)});
-      this.setState({response_hour: JSON.stringify(body[0][11])});
     }
   };
 
@@ -98,7 +89,16 @@ class App extends Component {
     var startHour = startDate.getHours();
     var openPlaces = JSON.parse(this.state.response)[startDay][startHour];
     this.setState({response_hour: JSON.stringify(openPlaces)})
-}
+  }
+
+  clearSearch = () => {
+    this.setState({response: ''});
+    this.setState({response_hour: ''});
+  }
+
+  clearCalendar = () => {
+    this.setState({events: []});
+  }
 
   addEvent = () => {
     // var updatedEvents = this.state.events;
@@ -122,16 +122,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Schedplore</h1>
+          <h1 className="App-title"> Schedplore </h1>
         </header>
 
-        {!this.state.response_hour &&
+        {!this.state.response &&
           <div>
-            <p className="App-intro">
-              To get started, search a city and select an hour.
-            </p>
-
             <PlacesAutocomplete
               value={this.state.address}
               onChange={this.handleChange}
@@ -140,53 +135,75 @@ class App extends Component {
             >
               {({ getInputProps, suggestions, getSuggestionItemProps }) => (
                 <div>
-                  <input
-                    {...getInputProps({
-                      placeholder: 'Search Places ...',
-                      className: 'location-search-input',
-                      id: 'city'
-                    })}
-                  />
-                  <div className="autocomplete-dropdown-container">
-                    {suggestions.map(suggestion => {
-                      const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                      // inline style for demonstration purpose
-                      const style = suggestion.active
-                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                      return (
-                        <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                          <span>{suggestion.description}</span>
-                        </div>
-                      )
-                    })}
+                  <div className="row">
+                    <div className="col-md-4 offset-md-4 mt-5">
+                      <div className="input-group">
+                        <input
+                          {...getInputProps({
+                            placeholder: 'Search for a City',
+                            className: 'form-control location-search-input',
+                            id: 'city',
+                          })}
+                        />
+                        <span className="input-group-btn">
+                          <button className="btn btn-primary" onClick={this.callApi}> Search </button>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="autocomplete-dropdown-container col-md-4 offset-md-4">
+                      {suggestions.map(suggestion => {
+                        const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                        // inline style for demonstration purpose
+                        const style = suggestion.active
+                          ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                          : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                        return (
+                          <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                            <span>{suggestion.description}</span>
+                          </div>
+                        )
+                      })}
+                    </div>
                   </div>
                 </div>
               )}
 
             </PlacesAutocomplete>
-            <button type="submit" onClick={this.callApi}>Search</button>     
           </div>   
         }
         
-        {this.state.response_hour &&
-          <div class="row cal-and-list">
-            <div class="big-cal col-md-9">
-              <BigCalendar
-                selectable
-                onSelectSlot={(slotInfo) => this.onSlotChange(slotInfo) }
-                localizer={localizer}
-                events={this.state.events}
-                defaultDate={new Date()}
-                defaultView="week"
-              />
-            </div>
-            
-            <div class="place-list-view col-md-3">
-              {this.state.response_hour &&
-                <PlaceList places={this.state.response_hour} updateCalendar={this.updateCalendar}/>
-              }
-            </div>
+        {this.state.response &&
+          <div>
+            <button className="btn btn-light back-button" onClick={this.clearSearch}> Back to Search </button>
+            <button className="btn btn-warning clear-calendar" onClick={this.clearCalendar}> Clear Calendar </button>
+            <div className="row cal-and-list">
+              <div className="big-cal-container col-md-9">
+                <div className="big-cal">
+                  <BigCalendar
+                    selectable
+                    onSelectSlot={(slotInfo) => this.onSlotChange(slotInfo) }
+                    localizer={localizer}
+                    events={this.state.events}
+                    defaultDate={new Date()}
+                    defaultView="week"
+                  />
+                </div>
+              </div>
+              <div className="place-list-view col-md-3">
+                <div className="place-list">
+                  {this.state.response_hour &&
+                    <PlaceList places={this.state.response_hour} updateCalendar={this.updateCalendar}/>
+                  }
+                </div>
+                {!this.state.response_hour &&
+                    <div className="emptyListMessage"> 
+                      <p className="emptyListText"> Select a time slot to view open restaurants! </p>
+                    </div>
+                  }
+              </div>
+            </div>                  
           </div>
         }
         
